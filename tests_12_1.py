d04282a12bc2e46b53b718e54c24da6e39afd624
Woylufh <165782530+Woylufh@users.noreply.github.com>
2024-07-30 21:52:32 +0700

    firste

---
+++ tests_12_1.py
@@ -0,0 +1,30 @@
+from runner import Runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_wolk(self):
+        masha = Runner(name='Маша')
+        for i in range(10):
+            i = masha.walk()
+        self.assertEqual(i, 50)
+
+    def test_run(self):
+        kolya = Runner(name='Коля')
+        for i in range(10):
+            i = kolya.run()
+        self.assertEqual(i, 100)
+
+    def test_challenge(self):
+        kolya = Runner(name='Коля')
+        masha = Runner(name='Маша')
+        for i in range(10):
+            i = kolya.walk() and kolya.run()
+        self.assertNotEqual(i, 100)
+        for i in range(10):
+            i = masha.walk() and masha.run()
+        self.assertNotEqual(i, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
